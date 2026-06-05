239e4717212f021790da48b2a6a488a6a5fc51fb
jasti-keerthi <keerthanajasti69@gmail.com>
2026-06-05 09:36:26 +0530

    Add files via upload

---
+++ prg90.py
@@ -0,0 +1,10 @@
+Lang={
+    'UI/UX':['HTML','CSS','JAVASCRIPT','REACT JS','EXPRESS JS'],
+    'Backend':['Python','Flask','Django','Pytamid','Bottel'],
+    'Server' :['SQL','Pl-Sql','Ma-SQL']
+}
+print(Lang['UI/UX'])
+print(Lang['Backend'])
+print(Lang['Server'])
+Lang['Server']=['Oracle-SQL']
+print(Lang['Server'])
